efaf42dae1eece7f18f1ca0833b16f915012053b
sukhwan Joung <dave@jober.io>
2018-08-06 21:01:32 +0900

    complete reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // 1
+  // return str.split('').reverse().join('');
+
+  // 2
+  // const n = str.length;
+  // let reversed = '';
+  // for (let i = 0; i < n; i++) {
+  //   reversed = str[i] + reversed;
+  // }
+  // return reversed;
+
+  // 3
+  // let reversed = '';
+  // for (let character of str){
+  //   reversed = character + reversed;
+  // }
+  // return reversed;
+
+  // 4
+  // return str.split('').reduce((reversed, character) => {
+  //   return character + reversed;
+  // },'');
+
+  // 5
+  return str.split('').reduce((rev, char) => char + rev,'');
+}
 
 module.exports = reverse;
